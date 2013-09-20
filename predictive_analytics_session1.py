896fe66069c60d91e1776d3846221bc279471c20
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2013-09-20 13:17:27 +0300

    Plotting functions went to a separate file.

--- predictive_analytics_session1.py
+++ predictive_analytics_session1.py
@@ -4,6 +4,7 @@ import statsmodels.api as sm
 import pylab as pl
 import numpy as np
 
+from plots import *
 
 # Read the data in (edit the path if necessary)
 # Notice: The last option is needed since the data file do not include row indices.
@@ -32,37 +33,7 @@ y = np.array(df['wine_color']=="red", int)
 # Summarize the data
 print X.describe()
 
-
-
-
-## Plot histogram of all the variables
-def hcolsrows(X):
-    cols = int(np.ceil(np.sqrt(len(X.columns))))
-    rows = int(np.ceil(len(X.columns) / float(cols)))
-    return cols, rows
-
-def plot_histograms(X, no_bins=50):
-    cols, rows = hcolsrows(X)
-    for count, vi in enumerate(X.columns.values):
-        pl.subplot(rows, cols, count+1)
-        pl.hist(X[vi], bins = no_bins, color = 'b', alpha=0.5, edgecolor='b', histtype="stepfilled")
-        pl.title(vi)
-    pl.show()
-
-
 plot_histograms(X)
-
-
-## Plot histograms of all the variables (target classes separately)
-def plot_class_hists(y, X, no_bins=50, alpha=0.5):
-    cols, rows = hcolsrows(X)
-    for count, vi in enumerate(X.columns.values):
-        pl.subplot(rows, cols, count+1)
-        pl.hist(X[vi][y==0], bins = no_bins, color = 'g', alpha=alpha, edgecolor='g', histtype="stepfilled")
-        pl.hist(X[vi][y==1], bins = no_bins, color = 'r', alpha=alpha, edgecolor='r', histtype="stepfilled")
-        pl.title(vi)
-    pl.show()
-
 plot_class_hists(y, X, alpha=.3)
 
 
@@ -85,13 +56,6 @@ var1, var2 = 'fixed_acidity', 'chlorides'
 
 ## Plot scatter plot of the two most important variables
 # Leave the possibility to transform variables, set initially to identity function.
-_1 = lambda x: x
-def plot_scatter(y, X, var1, var2, f1=_1, f2=_1, alpha=0.5, xmax=None, ymax=None):
-    pl.plot(f1(X[var1][y==0]), f2(X[var2][y==0]), 'g^', markeredgecolor='g', alpha=alpha)
-    pl.plot(f1(X[var1][y==1]), f2(X[var2][y==1]), 'r*', markeredgecolor='r', alpha=alpha)
-    pl.xlabel(var1)
-    pl.ylabel(var2)
-    pl.show()
 
 plot_scatter(y, X, var1, var2, alpha=0.2, ymax=0.4)
 plot_scatter(y, X, var1, var2, np.log, np.log, alpha=0.2, ymax=0.4)
@@ -116,22 +80,6 @@ print prediction_accuracy(y, p)
 def vargrid(x, n=300):
 	x0, x1 = min(x), max(x); return np.arange(x0, x1, (x1 - x0)/float(n))
 
-## Visualize the result
-def visualize_result_2d_linear(y, X, ml, var1, var2, alpha=0.25):
-    # Set up a 2d grid
-    x1g, x2g = np.meshgrid(vargrid(X[var1]), vargrid(X[var2]))
-    Xg = pd.DataFrame({var1 : np.ravel(x1g), var2 : np.ravel(x2g), 'intercept' : 1.0})
-    # Draw predictions below
-    p = ml.predict(Xg[[var1, var2, 'intercept']])
-    #h = pl.contourf(x1g, x2g, np.reshape(p, x1g.shape), 500, alpha=0.2)
-    h = pl.contourf(x1g, x2g, np.reshape(p, x1g.shape), 300, cmap=pl.cm.gist_yarg)
-    cbar = pl.colorbar()
-    cbar.set_label('probability for y=1')
-    # ... and data above.
-    pl.plot(X[var1][y==0], X[var2][y==0], 'go', markeredgecolor='g', alpha=alpha)
-    pl.plot(X[var1][y==1], X[var2][y==1], 'ro', markeredgecolor='r', alpha=alpha)
-    pl.xlabel(var1); pl.ylabel(var2)
-    pl.show()
 
 visualize_result_2d_linear(y, X, mfit, var1, var2)
 # In figure, there is a zoom tool that you may use to change axis limits.
