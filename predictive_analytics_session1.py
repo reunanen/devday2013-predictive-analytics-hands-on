8591a2b5b26bbee6847a5c445c8a0c6d42664d5c
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2013-09-20 14:58:17 +0300

    More comments to the main script

--- predictive_analytics_session1.py
+++ predictive_analytics_session1.py
@@ -74,52 +74,74 @@ def pacc(y, p): return 100*sum(y == (p>0.5)) / float(len(y))
 # Predictions: what the model "remembers" about the wines in the training data,
 # are the wines red or white (probabilities for the wine being red)
 p = mfit.predict(X[vars2d])
-# How those probabilities compare to what the wines actually were.
+# How those probabilities compare to what the wines actually were?
 # There are many ways to evaluate the predictions, here we just
 # see how many hits are correct if probabilities over 0.5 are guessed as red.
 print pacc(y, p)
 
 
-# [FIXME: explain...]
+# From the model's point of view, all possible wines are defined by all possible values
+# of the predictors. Let's look at what the model thinks about different possible wines.
 # Note the zoom tool in the fig!
 visualize_result_2d_linear(y, X, mfit, var1, var2)
 
+# Note the linear decision boundary above. This comes from the linearity of the
+# logistic regression. 
 
-# FIXME: I'm here /JSi
-# Construct a more complicated model using nonlinear transformations of the original variables
+# If you want a more flexible decision boundary, you can add nonlinear terms
+# to the model.
+# Then the model is still linear with respect to variables it sees, but not
+# with respect to the original data!
 def add_mterm(X, vars):
+    """Add a multiplicative term to the model."""
     X = X.copy()
     X['*'.join(vars)] = np.multiply.reduce(X[list(vars)].values, 1)
     return X
 
+# Here are some multiplicative terms.
+# For example, (var1, var1) will add the term var1*var1 to the model,
+# so the model would be var1 + var2 + var1*var2 + intercept
 nonlins = [(var1, var1), (var2, var2), (var1, var1, var1), (var2, var2, var2), (var1, var2)]
+# Let's make a data with all the nonlinear terms above!
 Xn = reduce(add_mterm, nonlins, X[vars2d])
 
-## Fit the model using nonlinear transformations
-m_nl = sm.Logit(y,Xn)
-mfit_nl = m_nl.fit()
+# Fit the model with all the new nonlinear terms. 
+m_nl = sm.Logit(y, Xn) 			# Model object with the expanded set of predictors. 
+mfit_nl = m_nl.fit()			# Fit it. 
+print mfit_nl.summary()			# See how it is
 
-# Evaluate the prediction accuracy
+# Probabilities the new model gives to wines being red
+# Hint: you may plot p_nl against the predictions of the linear model (p).
 p_nl = mfit_nl.predict(Xn)
+# How many it gets right?
 print pacc(y, p_nl)
 
-## Visualize the result (nonlinear decision boundary)
+# Visualize the probabilities (decision boundary) of the nonlinear model.
+# Set up a grid of values of the original variables 
 x1g, x2g = np.meshgrid(vargrid(X[var1]), vargrid(X[var2]))
+# Make a data frame out of the values on the grid. 
 Xg = pd.DataFrame({var1 : np.ravel(x1g)})
+# FIXME: the order is not important, collapse into a pdf.DataFrame() call.
 Xg[var2] = np.ravel(x2g)
 Xg['intercept'] = 1.0
+# Expand the data frame by computing all the nonlinearities.
 Xg = reduce(add_mterm, nonlins, Xg)
+# Make sure the order of the terms is as in the original data
+Xg = Xg[Xn.columns]
+# Compute predictions
 pg = mfit_nl.predict(Xg[Xn.columns])
 
-#h = pl.contourf(x1, x2, np.reshape(pg, x1g.shape),500, alpha=0.85)
+# Plot them
 h = pl.contourf(x1g, x2g, np.reshape(pg, x1g.shape), 300, cmap=pl.cm.gist_yarg)
 cbar = pl.colorbar()
 cbar.set_label('probability for y=1')
+# Overlay the data on top of predictions
 pl.plot(X[var1][y==0], X[var2][y==0], 'g^', markeredgecolor='g', alpha=0.5)
 pl.plot(X[var1][y==1], X[var2][y==1], 'r*', markeredgecolor='r', alpha=0.5)
 pl.xlabel(var1); pl.ylabel(var2)
 pl.show()
+# Again, note the zoom tool in the plot window.
 
-# In figure, there is a zoom tool that you may use to change axis limits.
+
 
 
